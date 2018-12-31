6c58068e1ad13ea11f55f050f9573026976a7443
Essien Saviour <IbibioDeveloper@Wisdomanthonis-MacBook-Pro-2.local>
2018-12-31 18:42:53 +0100

    Initial commit

---
+++ build/precache-manifest.7a101f30ae55cf23b8a21f86f0c1f9ef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d87cf68078df351cb278",
+    "url": "/static/css/main.7faafa26.chunk.css"
+  },
+  {
+    "revision": "d87cf68078df351cb278",
+    "url": "/static/js/main.d87cf680.chunk.js"
+  },
+  {
+    "revision": "2e622b3e03962dbb3d99",
+    "url": "/static/css/1.844ed698.chunk.css"
+  },
+  {
+    "revision": "2e622b3e03962dbb3d99",
+    "url": "/static/js/1.2e622b3e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ad17c6085dee9a33787bac28fb23d46",
+    "url": "/static/media/glyphicons-halflings-regular.7ad17c60.eot"
+  },
+  {
+    "revision": "68ed1dac06bf0409c18ae7bc62889170",
+    "url": "/static/media/glyphicons-halflings-regular.68ed1dac.woff"
+  },
+  {
+    "revision": "e49d52e74b7689a0727def99da31f3eb",
+    "url": "/static/media/glyphicons-halflings-regular.e49d52e7.ttf"
+  },
+  {
+    "revision": "ff423a4251cf2986555523dfe315c42b",
+    "url": "/static/media/glyphicons-halflings-regular.ff423a42.svg"
+  },
+  {
+    "revision": "b185066a39fd4d19e78c2d69d8cedfef",
+    "url": "/static/media/sequence.b185066a.jpeg"
+  },
+  {
+    "revision": "efed833281e31fa774153b0028b5f3d7",
+    "url": "/index.html"
+  }
+];
