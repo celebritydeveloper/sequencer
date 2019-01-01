a52c600c811a775b85cd59e8d7eb3a29440f7f52
Essien Saviour <IbibioDeveloper@Wisdomanthonis-MacBook-Pro-2.local>
2019-01-01 10:46:39 +0100

    changed gitignore

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90b44719b7a2a0184b7b2342472024b9.js"
+  "/sequencer/precache-manifest.abab89bb0af572c1ffcff6a49cf2354d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/sequencer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
